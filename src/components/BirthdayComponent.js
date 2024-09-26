de342c542bf625dd19e463768cbc0df57348e869
pavankumar19992208 <pavankumar19992208@gmail.com>
2024-09-26 15:36:57 +0530

    ...

--- src/components/BirthdayComponent.js
+++ src/components/BirthdayComponent.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import Confetti from 'react-confetti';
 import './new.css';
 
+// Import images
+import img1 from '../images/img1.png';
+import img2 from '../images/img2.jpg';
+import img3 from '../images/img3.jpg';
+
 const BirthdayComponent = () => {
   const [showConfetti, setShowConfetti] = useState(false);
 
@@ -28,12 +33,21 @@ const BirthdayComponent = () => {
       <h1 style={{ color: '#181818' }}>.fhohfd;hnfkfjhnsfhnsfnf</h1>
       <div className="animation-box">
         <div className="first-text">“Happy birthday! I hope all your birthday wishes and dreams come true.”</div>
-        <div className="second-text">“A wish for you on your birthday, whatever you ask may you receive, whatever you seek may you find, whatever you wish may it be fulfilled on your birthday and always. Happy birthday!”</div>
+        <div className="second-text">
+          “A wish for you on your birthday, whatever you ask may you receive, whatever you seek may you find, whatever you wish may it be fulfilled on your birthday and always. Happy birthday!”<br/>
+          {/* <img src={img1} className="image-animation1" alt="Birthday Image 1" onError={(e) => { console.error('Error loading img1:', e); }} /> */}
+        </div>
         <div className="third-text">“May the joy that you have spread in the past come back to you on this day. Wishing you a very happy birthday!”</div>
-        <div className="fourth-text">“Count not the candles…see the lights they give. Count not the years, but the life you live. Wishing you a wonderful time ahead. Happy birthday.”</div>
+        <div className="fourth-text">
+          “Count not the candles…see the lights they give. Count not the years, but the life you live. Wishing you a wonderful time ahead. Happy birthday.”
+          {/* <img src={img2} className="image-animation2" alt="Birthday Image 2" onError={(e) => { console.error('Error loading img2:', e); }} /> */}
+        </div>
         <div className="fifth-text" style={{ fontSize: '36px' }}>“Be happy! Today is the day you were brought into this world to be a blessing and inspiration to the people around you! You are a wonderful person! May you be given more birthdays to fulfill all of your dreams!”</div>
         {showConfetti && <Confetti />}
-        <div className="sixth-text">HAPPY <br />BIRTHDAY</div>
+        <div className="sixth-text">
+          HAPPY <br />BIRTHDAY <br/> <span className='name'>Vanitha</span>
+          <img src={img3} className="image-animation3" alt="Birthday Image 3" onError={(e) => { console.error('Error loading img3:', e); }} />
+        </div>
         <div className="seventh-text">-Pramod</div>
       </div>
     </div>
