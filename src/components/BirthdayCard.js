159796a059f05b0322231ce8ae2912d6f1d35c5e
pavankumar19992208 <pavankumar19992208@gmail.com>
2024-09-27 10:47:19 +0530

    ././

--- src/components/BirthdayCard.js
+++ src/components/BirthdayCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Typography } from '@mui/material';
 import { TouchApp } from '@mui/icons-material';
@@ -8,47 +8,54 @@ import { useAudio } from './AudioContext';
 const BirthdayCard = () => {
   const navigate = useNavigate();
   const { playAudio } = useAudio();
+  const [isEnvelopeOpened, setIsEnvelopeOpened] = useState(false);
 
   const handleClick = () => {
     playAudio();
     navigate('/birthdaycomponent');
   };
 
+  const handleEnvelopeOpen = () => {
+    setIsEnvelopeOpened(true);
+  };
+
   return (
     <div style={{ backgroundColor: '#fff4ec', width: '100vh', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <div className="envelope">
+      <div className="envelope" onClick={handleEnvelopeOpen}>
         <div className="back"></div>
         <div className="letter">
           <div className='heart'></div>
           <div className="text">
-            <p>hello! &hearts;</p>
-            <Button
-              className="button"
-              onClick={handleClick}
-              variant="contained"
-              color="primary"
-              style={{
-                position: 'relative',
-                padding: '8px 20px',
-                background: '#fec195',
-                fontSize: '12px',
-                fontWeight: '500',
-                color: '#181818',
-                cursor: 'pointer',
-                border: '1px solid #fec195',
-                borderRadius: '16px',
-                filter: 'drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.2))'
-              }}
-            >
-              Click here
-            </Button>            
+            <p>Happy to meet you again Vanitha. I have something for your special Day. &hearts;</p>
+            {isEnvelopeOpened && (
+              <Button
+                className="button"
+                onClick={handleClick}
+                variant="contained"
+                color="primary"
+                style={{
+                  position: 'relative',
+                  padding: '8px 20px',
+                  background: '#fec195',
+                  fontSize: '12px',
+                  fontWeight: '500',
+                  color: '#181818',
+                  cursor: 'pointer',
+                  border: '1px solid #fec195',
+                  borderRadius: '16px',
+                  filter: 'drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.2))'
+                }}
+              >
+                Gift
+              </Button>
+            )}
           </div>
         </div>
         <div className="front"></div>
         <div className="top"></div>
         <div className="shadow"></div>
         <div className="text1">
-          <h2>Hey! Listen I have something for you from my heart.</h2>
+          {/* <h2>Hey! Listen I have something for you from my heart.</h2> */}
         </div>
         <div className="tap-here">
           <Typography variant="h6" component="p" className="tap-text">
